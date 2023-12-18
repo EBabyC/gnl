564a8c5e0a5dc4b780d81dd4ae9267cdf1441308
Elisa Chan <elichan@bess-f1r1s8.clusters.42paris.fr>
2023-12-18 15:06:04 +0100

    elisa

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elichan < elichan@student.42.fr >          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 14:44:32 by elichan           #+#    #+#             */
+/*   Updated: 2023/12/18 14:47:11 by elichan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
